fa41433a2e7c4e4bbc01fd5a654b7d6330fc76c0
sanjay kumar <sanjaykumar034831@gmail.com>
2023-04-22 09:03:09 +0500

    updated cors setting

--- src/main/java/com/tms/loader/LoaderApplication.java
+++ src/main/java/com/tms/loader/LoaderApplication.java
@@ -4,6 +4,8 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class LoaderApplication {
@@ -16,5 +18,14 @@ public class LoaderApplication {
 	ModelMapper modelMapper() {
 		return new ModelMapper();
 	}
+	public WebMvcConfigurer configure() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/*").allowedOriginPatterns("http://localhost:3000");
+			}
+		};
+	}
+	
 	
 }
