86e80d28dff7c09de0eca842fe1427eb30a077d3
sanjay kumar <sanjaykumar034831@gmail.com>
2023-05-17 13:23:11 +0500

    added new allowed origin

--- src/main/java/com/tms/loader/LoaderApplication.java
+++ src/main/java/com/tms/loader/LoaderApplication.java
@@ -24,7 +24,7 @@ public class LoaderApplication {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("http://localhost:3000", "https://loaderadmin.netlify.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE");
             }
         };
